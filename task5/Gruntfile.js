aa19c805c71730381cfcef5d1dd126c15ee2e91d
niksoper <nsoper@scottlogic.com>
2014-06-24 10:15:34 +0100

    Set up grunt to run app server and test server.

--- task5/Gruntfile.js
+++ task5/Gruntfile.js
@@ -4,11 +4,14 @@ module.exports = function (grunt) {
 
     'use strict';
 
-    // load the tasks
-    grunt.loadNpmTasks("grunt-ts");
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    var LIVERELOAD_PORT = 35729;
+    var lrSnippet = require('connect-livereload')({
+      port: LIVERELOAD_PORT
+    });
+
+    var mountFolder = function(connect, dir) {
+        return connect.static(require('path').resolve(dir));
+    };
 
     // Configure grunt here
     grunt.initConfig({
@@ -32,23 +35,30 @@ module.exports = function (grunt) {
                 }]
             }
         },
-        watch: {
-            css: {
-                files: 'app/scss/*.scss',
-                tasks: ['sass']
-            },
-            ts: {
-                files: 'app/ts/**/*.ts',
-                tasks: ['ts:dev']
-            }
-        },
         connect: {
-            test : {
-                port : 8000
+            test: {
+                options: {
+                    port: 9000,
+                    base: '.',
+                    hostname: 'localhost',
+                    middleware: function(connect) {
+                        return [lrSnippet, mountFolder(connect, '.')];
+                    }
+                }
+            },
+            app: {
+                options: {
+                    port: 8080,
+                    base: 'app',
+                    hostname: 'localhost',
+                    middleware: function(connect) {
+                        return [connect.logger('dev'), mountFolder(connect, 'app')];
+                    }
+                }
             }
         },
         jasmine: {
-            taskName: {
+            unit: {
                 src: 'app/ts/**/*.js',
                 options: {
                     specs: 'test/unit/*Spec.js',
@@ -59,8 +69,39 @@ module.exports = function (grunt) {
                     }
                 }
             }
-        }
+        },
+        open: {
+            dev: {
+                path: 'http://localhost:<%= connect.test.options.port %>/_SpecRunner.html'
+            }
+        },
+        watch: {
+            css: {
+                files: 'app/scss/*.scss',
+                tasks: ['sass']
+            },
+            ts: {
+                files: 'app/ts/**/*.ts',
+                tasks: ['ts:dev']
+            },
+            tests: {
+                files: ['<%= jasmine.unit.src %>', '<%= jasmine.unit.options.specs %>']
+            },
+            options: {
+                livereload: true
+            }
+        },
+        
     });
 
-    grunt.registerTask('default', ['ts:dev', 'sass', 'watch']);
+    // load the tasks
+    grunt.loadNpmTasks("grunt-ts");
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-open');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('connect-livereload');
+
+    grunt.registerTask('default', ['ts:dev', 'sass', 'connect:app', 'connect:test', 'open', 'watch']);
 };
--- task5/app/server.js
+++
@@ -1,9 +0,0 @@
-var connect = require('connect');
-
-var port = 8080;
-
-connect()
-.use(connect.logger('dev'))
-.use(connect.static(__dirname)).listen(8080);
-
-console.log('Server started listening on port ' + port);
